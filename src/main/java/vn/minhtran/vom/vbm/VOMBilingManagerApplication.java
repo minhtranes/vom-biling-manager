7673e0e97931538e3b2b5672a1168bf0f97e8cb6
Minh Tran <thminh_1@spsvietnam.vn>
2020-11-13 21:57:23 +0700

    Build data sources

--- src/main/java/vn/minhtran/vom/vbm/VOMBilingManagerApplication.java
+++ src/main/java/vn/minhtran/vom/vbm/VOMBilingManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VomBilingManagerApplication {
+public class VOMBilingManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VomBilingManagerApplication.class, args);
+		SpringApplication.run(VOMBilingManagerApplication.class, args);
 	}
 
 }
---
+++ src/main/java/vn/minhtran/vom/vbm/infra/entity/InvoiceEntity.java
@@ -0,0 +1,190 @@
+/*
+ * Class: InvoiceEntity
+ *
+ * Created on Nov 13, 2020
+ *
+ * (c) Copyright Swiss Post Solutions Ltd, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.minhtran.vom.vbm.infra.entity;
+
+import javax.persistence.Table;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "vom_invoices")
+@Access(AccessType.PROPERTY)
+public class InvoiceEntity implements Serializable {
+    
+    private static final long serialVersionUID = -3228223901723345675L;
+
+    private Integer id;
+
+    private String name;
+
+    private String rawData;
+
+    private String guestName;
+
+    private LocalDateTime checkInDate;
+
+    private LocalDateTime checkOutDate;
+
+    private String country;
+
+    private String rooms;
+
+    private Integer totalAmount;
+
+    private Integer paidAmount;
+
+    private Integer remainAmount;
+
+    private String pdfLink;
+
+    private LocalDateTime createdTime;
+
+    private LocalDateTime lastModifiedTime;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "invoice_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "data")
+    public String getRawData() {
+        return rawData;
+    }
+
+    public void setRawData(String rawData) {
+        this.rawData = rawData;
+    }
+
+    @Column(name = "guest_name")
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    @Column(name = "check_in_date")
+    public LocalDateTime getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDateTime checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    @Column(name = "check_out_date")
+    public LocalDateTime getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDateTime checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Column(name = "country")
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Column(name = "rooms")
+    public String getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(String rooms) {
+        this.rooms = rooms;
+    }
+
+    @Column(name = "total_amount")
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Column(name = "paid_amount")
+    public Integer getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(Integer paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+
+    @Column(name = "remain_amount")
+    public Integer getRemainAmount() {
+        return remainAmount;
+    }
+
+    public void setRemainAmount(Integer remainAmount) {
+        this.remainAmount = remainAmount;
+    }
+
+    @Column(name = "link")
+    public String getPdfLink() {
+        return pdfLink;
+    }
+
+    public void setPdfLink(String pdfLink) {
+        this.pdfLink = pdfLink;
+    }
+
+    @Column(name = "created_time")
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    @Column(name = "last_modified_time")
+    public LocalDateTime getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public void setLastModifiedTime(LocalDateTime lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
+    }
+
+}
