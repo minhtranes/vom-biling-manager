b3e8cd0fcf752a842c4526c76d64cb6c3d0ee094
Minh Tran <thminh_1@spsvietnam.vn>
2020-11-13 21:41:26 +0700

    Initialize project

---
+++ src/main/java/vn/minhtran/vom/vbm/VomBilingManagerApplication.java
@@ -0,0 +1,13 @@
+package vn.minhtran.vom.vbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VomBilingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VomBilingManagerApplication.class, args);
+	}
+
+}
