843e2a45dd2702bfc474030f0bced20d463f2136
Minh Tran <thminh_1@spsvietnam.vn>
2020-11-23 23:08:40 +0700

    Support DynamoDB

---
+++ src/main/java/vn/minhtran/vom/vbm/infra/dynamodb/repository/InvoiceRepository.java
@@ -0,0 +1,26 @@
+/*
+ * Class: InvoiceRepository
+ *
+ * Created on Nov 13, 2020
+ *
+ * (c) Copyright Swiss Post Solutions Ltd, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.minhtran.vom.vbm.infra.dynamodb.repository;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import vn.minhtran.vom.vbm.infra.dynamodb.entity.InvoiceEntity;
+
+
+@EnableScan
+public interface InvoiceRepository
+        extends JpaRepository<InvoiceEntity, Integer> {
+    
+    InvoiceEntity findOneByName(String name);
+}
